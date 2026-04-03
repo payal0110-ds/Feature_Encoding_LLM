00e3e6810a1858524fc834184c07279b334ff640
Payal Parida <payalparida@Payals-MacBook-Air.local>
2026-04-03 11:55:38 +0530

    Bag of Words code to transform the categorical data into numerical vector form.

--- bow.py
+++ bow.py
@@ -1,2 +1,32 @@
-a=8
-print(a)
+from nltk import word_tokenize
+from nltk import sent_tokenize
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+import numpy as np
+import re
+
+messages=pd.read_csv("SMSSpamCollection",sep="\t",names=["label","message"])
+
+corpus=[]
+for i in range(len(messages)):
+    review=re.sub("[^a-zA-Z]"," ",messages["message"][i])
+    review=review.lower()
+    review=review.split()
+    review=[WordNetLemmatizer().lemmatize(word) for word in review if word not in set(stopwords.words('english'))]
+    review=" ".join(review)
+    corpus.append(review)
+
+# print(corpus)
+print(len(corpus))
+
+cv=CountVectorizer(max_features=100,binary=True)
+"""
+Using 'CountVectorizer' also, we can lower and use stopwords directly.
+"""
+x=cv.fit_transform(corpus).toarray()
+np.set_printoptions(edgeitems=30, linewidth=100000, 
+    formatter=dict(float=lambda x: "%.3g" % x))
+
+print(x)
