0bac3734c4c75acb689db42d457c8138a3b704f4
Payal Parida <payalparida@Payals-MacBook-Air.local>
2026-04-03 12:08:38 +0530

    Bag of Words code to convert the text data into numerical vector form based on most frequency of word/token.

--- bow.py
+++ bow.py
@@ -21,6 +21,9 @@ for i in range(len(messages)):
 print(len(corpus))
 
 cv=CountVectorizer(max_features=100,binary=True)
+"""
+Using 'CountVectorizer' also, we can lower case and use stop words directly.
+"""
 x=cv.fit_transform(corpus).toarray()
 np.set_printoptions(edgeitems=30, linewidth=100000, 
     formatter=dict(float=lambda x: "%.3g" % x))
